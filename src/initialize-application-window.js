b22f9297ff935311afe2eea2afb2385287de6bb3
jonian <jonian@hardpixel.eu>
2022-12-12 22:42:31 +0200

    update language-rust package
    replaces language-rust-bundled

--- src/initialize-application-window.js
+++ src/initialize-application-window.js
@@ -41,7 +41,7 @@ if (global.isGeneratingSnapshot) {
   require('language-html');
   require('language-javascript');
   require('language-ruby');
-  require('language-rust-bundled');
+  require('language-rust');
   require('language-typescript');
   require('line-ending-selector');
   require('link');
